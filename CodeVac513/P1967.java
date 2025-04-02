454aac045042a974c8479ece0ad6ccf346d6e4a6
codeVac513 <xssdsw@naver.com>
2025-04-03 00:31:26 +0900

    오타 수정

--- CodeVac513/P1967.java
+++ CodeVac513/P1967.java
@@ -18,7 +18,6 @@ public class P1967 {
         int n = Integer.parseInt(st.nextToken());
 
         tree = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
         for (int i = 1; i <= n; i++) {
             tree[i] = new ArrayList<>();
         }
