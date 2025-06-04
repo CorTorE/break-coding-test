9c409a2ad4c9e056cf7d3b4215dae6a2a151bbd2
BangDori <qudwns8865@naver.com>
2025-06-04 09:37:52 +0900

    코드 수정

--- bangdori/322.js
+++ bangdori/322.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var coinChange = function (coins, amount) {
-  const dp = Array(amount + 1).fill(Infinity);
+  const dp = Array(amount + 1).fill(amount + 1);
   dp[amount] = 0;
-
-  coins.sort((a, b) => b - a);
 
   for (const coin of coins) {
     for (let i = amount; i >= coin; i--) {
-      dp[i - coin] = Math.min(dp[i - coin], dp[i] + 1);
+      if (dp[i - coin] > dp[i] + 1) {
+        dp[i - coin] = dp[i] + 1;
+      }
     }
   }
 
-  return dp[0] === Infinity ? -1 : dp[0];
+  return dp[0] > amount ? -1 : dp[0];
 };
